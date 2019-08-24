d9e2bbf6e02cf8312ed5126c572a7999de28c642
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-24 13:02:15 +0900

    形態素解析結果を保持するデータクラスParseResultData.ktを作成

---
+++ src/dialect/ParseResultData.kt
@@ -0,0 +1,24 @@
+package dialect
+
+data class ParseResultData(
+    // 表層系
+    val surface: String,
+    // 品詞
+    val lexicaCategory: String,
+    // 品詞細分類1
+    val lexicaCategoryClassification1: String,
+    // 品詞細分類2
+    val lexicaCategoryClassification2: String,
+    // 品詞細分類3
+    val lexicaCategoryClassification3: String,
+    // 活用形
+    val conjugationalForm: String,
+    // 活用型
+    val conjugationalType: String,
+    // 原形
+    val originalPattern: String,
+    // 読み
+    val reading: String,
+    // 発音
+    val pronunciation: String
+)
