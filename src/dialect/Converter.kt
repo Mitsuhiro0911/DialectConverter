f40a636d7aaa22158bb6ea8d3b8d7bc3776eeecf
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-26 15:34:36 +0900

    名詞の遠州弁変換処理をメソッド化

--- src/dialect/Converter.kt
+++ src/dialect/Converter.kt
@@ -7,11 +7,9 @@ class Converter {
     private val reader = SAXReader()
     private val document = reader.read("./data/corpas/dialect_data.xml")
     private val cp = ContextProcessor()
+    private val convertedText = ArrayList<String>()
 
     fun convert (parsedDataList: ArrayList<ParseResultData>) {
-        val convertedText = ArrayList<String>()
-        // lexicaCategoryが名詞 且つ importanceが3のstandard(標準語)情報を抽出
-        val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='名詞']][../importance[text()='3']]")
         for (parsedData in parsedDataList) {
             var convertedFlag = false
             // 接尾辞の場合、直前の単語の処理で纏めて解析しているため、処理をスキップ
@@ -20,19 +18,7 @@ class Converter {
             }
             // 名詞の変換
             if (parsedData.lexicaCategory == "名詞") {
-                // 接頭辞の結合処理
-                cp.appendPrefix(parsedDataList, parsedData, convertedText)
-                // 接尾辞の結合処理
-                cp.appnedSuffix(parsedDataList, parsedData)
-
-                for (standardWord in standardWordList) {
-                    if (standardWord.text == parsedData.surface) {
-                        // 標準語に対応した遠州弁を取得
-                        val ensyuWord: List<Node> = document.selectNodes("//enshu[../standard[text()='${standardWord.text}']]")
-                        convertedText.add(ensyuWord[0].text)
-                        convertedFlag = true
-                    }
-                }
+                convertedFlag = convertNoun(parsedDataList, parsedData)
             }
 
             // 遠州弁に変換されなかった単語はそのまま出力
@@ -43,5 +29,24 @@ class Converter {
         for (output in convertedText) {
             print(output)
         }
+    }
+    fun convertNoun (parsedDataList: ArrayList<ParseResultData>, parsedData: ParseResultData): Boolean{
+        var convertedFlag = false
+        // lexicaCategoryが名詞 且つ importanceが3のstandard(標準語)情報を抽出
+        val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='名詞']][../importance[text()='3']]")
+        // 接頭辞の結合処理
+        cp.appendPrefix(parsedDataList, parsedData, convertedText)
+        // 接尾辞の結合処理
+        cp.appnedSuffix(parsedDataList, parsedData)
+
+        for (standardWord in standardWordList) {
+            if (standardWord.text == parsedData.surface) {
+                // 標準語に対応した遠州弁を取得
+                val ensyuWord: List<Node> = document.selectNodes("//enshu[../standard[text()='${standardWord.text}']]")
+                convertedText.add(ensyuWord[0].text)
+                convertedFlag = true
+            }
+        }
+        return convertedFlag
     }
 }
