71e8a751d9a7213cab6468746549947b1e5a29d5
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-26 12:34:44 +0900

    標準語の名詞を遠州弁に変換する処理を実装

--- src/dialect/Converter.kt
+++ src/dialect/Converter.kt
@@ -10,13 +10,24 @@ class Converter {
     fun convert (parsedDataList: ArrayList<ParseResultData>) {
         // lexicaCategoryが品詞 且つ importanceが3のstandard(標準語)情報を抽出
         val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='名詞']][../importance[text()='3']]")
-        for (standardWord in standardWordList) {
-            println(standardWord.text)
-        }
         for (parsedData in parsedDataList) {
+            var convertedFlag = false
+            // 名詞の変換
             if (parsedData.lexicaCategory == "名詞") {
+                for (standardWord in standardWordList) {
+                    if (standardWord.text == parsedData.surface) {
+                        // 標準語に対応した遠州弁を取得
+                        val ensyuWord: List<Node> = document.selectNodes("//enshu[../standard[text()='${standardWord.text}']]")
+                        print(ensyuWord[0].text)
+                        convertedFlag = true
+                    }
+                }
+            }
+
+            // 遠州弁に変換されなかった単語はそのまま出力
+            if (!convertedFlag) {
+                print(parsedData.surface)
             }
         }
-
     }
 }
