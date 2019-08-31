9d7997771226967e6d24028ed099db694e49f1cf
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-31 13:13:53 +0900

    コードを再フォーマット。次データを保持する変数parsedNextDataを定義(リファクタリング)

--- src/dialect/Converter.kt
+++ src/dialect/Converter.kt
@@ -8,12 +8,19 @@ class Converter {
     private val document = reader.read("./data/corpas/dialect_data.xml")
     private val cp = ContextProcessor()
     private val convertedText = ArrayList<String>()
+    private var parsedNextData: ParseResultData? = null
 
     /**
      * 遠州弁変換メソッド群のハブ。形態素解析情報を元に、変換方式を決定する。
      */
-    fun convert (parsedDataList: ArrayList<ParseResultData>) {
+    fun convert(parsedDataList: ArrayList<ParseResultData>) {
         for (parsedData in parsedDataList) {
+            // parsedDataが末尾のデータでなければ、次データの情報を取得し、parsedNextDataへ格納
+            parsedNextData = null
+            if (parsedDataList.indexOf(parsedData) + 1 != parsedDataList.size) {
+                parsedNextData = parsedDataList[parsedDataList.indexOf(parsedData) + 1]
+            }
+
             var convertedFlag = false
             println(parsedData)
             // ルールでの変換が難しい単語を個別処理で変換
@@ -33,7 +40,7 @@ class Converter {
                     convertedFlag = convertNoun(parsedDataList, parsedData)
                 } else if (parsedData.lexicaCategory == "形容詞") {
                     convertedFlag = convertAdjective(parsedData)
-                } else if(parsedData.lexicaCategory == "助詞") {
+                } else if (parsedData.lexicaCategory == "助詞") {
                     convertedFlag = convertParticle(parsedData)
                 }
             }
@@ -51,18 +58,17 @@ class Converter {
     /**
      * 名詞を遠州弁に変換する。
      */
-    private fun convertNoun (parsedDataList: ArrayList<ParseResultData>, parsedData: ParseResultData): Boolean{
+    private fun convertNoun(parsedDataList: ArrayList<ParseResultData>, parsedData: ParseResultData): Boolean {
         var convertedFlag = false
         // lexicaCategoryが名詞 且つ importanceが3のstandard(標準語)情報を抽出
-        val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='名詞']][../importance[text()='3']]")
+        val standardWordList: List<Node> =
+            document.selectNodes("//standard[../lexicaCategory[text()='名詞']][../importance[text()='3']]")
         // 接頭辞の結合処理
         cp.appendPrefix(parsedDataList, parsedData, convertedText)
-        // inputTextの末尾の場合は接尾辞がつく可能性はないため接尾辞処理はしない
-        if (parsedDataList.indexOf(parsedData) + 1 != parsedDataList.size) {
+        // inputTextの末尾の場合は接尾辞処理はしない
+        if (parsedNextData != null) {
             // 直後が人名の接尾辞の場合
-            if (parsedDataList[parsedDataList.indexOf(parsedData) + 1].lexicaCategoryClassification1 == "接尾" && parsedDataList[parsedDataList.indexOf(
-                    parsedData
-                ) + 1].lexicaCategoryClassification2 == "人名"
+            if (parsedNextData!!.lexicaCategoryClassification1 == "接尾" && parsedNextData!!.lexicaCategoryClassification2 == "人名"
             ) {
                 // 接尾辞の結合処理
                 cp.appnedSuffix(parsedDataList, parsedData)
@@ -75,10 +81,11 @@ class Converter {
     /**
      * 形容詞を遠州弁に変換する。
      */
-    private fun convertAdjective (parsedData: ParseResultData): Boolean {
+    private fun convertAdjective(parsedData: ParseResultData): Boolean {
         var convertedFlag = false
         // lexicaCategoryが形容詞 且つ importanceが3のstandard(標準語)情報を抽出
-        val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='形容詞']][../importance[text()='3']]")
+        val standardWordList: List<Node> =
+            document.selectNodes("//standard[../lexicaCategory[text()='形容詞']][../importance[text()='3']]")
         convertedFlag = simplConvert(parsedData, standardWordList)
         return convertedFlag
     }
@@ -86,10 +93,11 @@ class Converter {
     /**
      * 副詞を遠州弁に変換する。
      */
-    private fun convertAdverb (parsedData: ParseResultData): Boolean {
+    private fun convertAdverb(parsedData: ParseResultData): Boolean {
         var convertedFlag = false
         // lexicaCategoryが副詞 且つ importanceが3のstandard(標準語)情報を抽出
-        val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='副詞']][../importance[text()='3']]")
+        val standardWordList: List<Node> =
+            document.selectNodes("//standard[../lexicaCategory[text()='副詞']][../importance[text()='3']]")
         convertedFlag = simplConvert(parsedData, standardWordList)
         return convertedFlag
     }
@@ -100,9 +108,11 @@ class Converter {
     private fun convertParticle(parsedData: ParseResultData): Boolean {
         var convertedFlag = false
         // lexicaCategoryが副詞 且つ importanceが3のstandard(標準語)情報を抽出
-        val standardWordList: List<Node> = document.selectNodes("//standard[../lexicaCategory[text()='助詞']][../importance[text()='3']]")
+        val standardWordList: List<Node> =
+            document.selectNodes("//standard[../lexicaCategory[text()='助詞']][../importance[text()='3']]")
         // 助詞がくっつく直前の単語を抽出
-        val preWordList: List<Node> = document.selectNodes("//pre_word[../lexicaCategory[text()='助詞']][../importance[text()='3']]")
+        val preWordList: List<Node> =
+            document.selectNodes("//pre_word[../lexicaCategory[text()='助詞']][../importance[text()='3']]")
         // 助詞がくっつく直前の単語が正しい時のみ遠州弁に変換する
         for (preword in preWordList) {
             if (preword.text == convertedText[convertedText.size - 1]) {
@@ -118,8 +128,10 @@ class Converter {
      * ルール化が難しい単語の個別変換処理
      */
     fun uniqueConvert(parsedData: ParseResultData): Boolean {
+        // TODO:「だら」は個別変換処理
         var convertedFlag = false
-        if(parsedData.surface == "ごと" && parsedData.lexicaCategoryClassification1 == "接尾") {
+        // 「ごと」→「さら」
+        if (parsedData.surface == "ごと" && parsedData.lexicaCategoryClassification1 == "接尾") {
             val ensyuWord: List<Node> = document.selectNodes("//enshu[../standard[text()='ごと']]")
             convertedText.add(ensyuWord[0].text)
             convertedFlag = true
@@ -130,7 +142,7 @@ class Converter {
     /**
      * 各変換メソッドから呼ばれる共通変換処理。
      */
-    private fun simplConvert (parsedData: ParseResultData, standardWordList: List<Node>): Boolean {
+    private fun simplConvert(parsedData: ParseResultData, standardWordList: List<Node>): Boolean {
         var convertedFlag = false
         for (standardWord in standardWordList) {
             if (standardWord.text == parsedData.surface) {
