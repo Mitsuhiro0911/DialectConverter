222a9dacc55684ca2e4a65b7c6eec3bfac728452
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-09-16 16:43:27 +0900

    ParseResultDataをBuilderパターンでリファクタリング

--- src/dialect/ParseResultData.kt
+++ src/dialect/ParseResultData.kt
@@ -1,6 +1,7 @@
 package dialect
 
 data class ParseResultData(
+    // インナークラスからアウタークラスに代入する(builderメソッド)際にvarをvalに変更できるため、保守性が向上できる。
     // 表層系
     var surface: String,
     // 品詞
@@ -20,5 +21,54 @@ data class ParseResultData(
     // 読み
     val reading: String,
     // 発音
-    val pronunciation: String
-)
+    val pronunciation: String) {
+
+    data class Builder(
+        var surface: String,
+        var lexicaCategory: String,
+        val lexicaCategoryClassification1: String,
+        val lexicaCategoryClassification2: String,
+        val lexicaCategoryClassification3: String,
+        val conjugationalForm: String,
+        val conjugationalType: String,
+        val originalPattern: String,
+        var reading: String = "*",
+        var pronunciation: String = "*") {
+
+        /**
+         * 「読み」の情報がある場合セットする
+         */
+        fun reading(parseResult: Array<String>) = apply {
+            if (parseResult.size > 8) {
+                reading = parseResult[8]
+            }
+        }
+
+        /**
+         * 「発音」の情報がある場合セットする
+         */
+        fun pronunciation(parseResult: Array<String>) = apply {
+            if (parseResult.size > 9) {
+                reading = parseResult[9]
+            }
+        }
+
+        /**
+         * ビルダーの実行
+         */
+        fun builder(): ParseResultData {
+            return ParseResultData(
+                surface,
+                lexicaCategory,
+                lexicaCategoryClassification1,
+                lexicaCategoryClassification2,
+                lexicaCategoryClassification3,
+                conjugationalForm,
+                conjugationalType,
+                originalPattern,
+                reading,
+                pronunciation
+            )
+        }
+    }
+}
--- src/dialect/Parser.kt
+++ src/dialect/Parser.kt
@@ -26,43 +26,19 @@ class Parser {
             } else if (targetLine == "EOS") {
                 continue
             } else {
-                // 品詞
-                val targetType =
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                val word = targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                val splitElementSize = targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray().size
-                val reading: String
-                val pronunciation: String
+                // 形態素解析結果
+                val parseResult = targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
                 // 読み、発音情報がない単語もある。その場合要素数が少ないため、[8][9]の要素を指定するとArrayIndexOutOfBoundsExceptionで落ちる。
-                if (splitElementSize > 8) {
-                    reading = targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[8]
-                    pronunciation = targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[9]
-                } else {
-                    reading = "*"
-                    pronunciation = "*"
-                }
-                val parsedData = ParseResultData(
-                    // 表層系
-                    targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0],
-                    // 品詞
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1],
-                    // 品詞細分類1
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[2],
-                    // 品詞細分類2
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[3],
-                    // 品詞細分類3
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[4],
-                    // 活用形
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[5],
-                    // 活用型
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[6],
-                    // 原形
-                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[7],
-                    // 読み
-                    reading,
-                    // 発音
-                    pronunciation
-                    )
+                val parsedData = ParseResultData.Builder(
+                    surface = parseResult[0],
+                    lexicaCategory = parseResult[1],
+                    lexicaCategoryClassification1 = parseResult[2],
+                    lexicaCategoryClassification2 = parseResult[3],
+                    lexicaCategoryClassification3 = parseResult[4],
+                    conjugationalForm = parseResult[5],
+                    conjugationalType = parseResult[6],
+                    originalPattern = parseResult[7]
+                    ).reading(parseResult).pronunciation(parseResult).builder()
                 parsedDataList.add(parsedData)
             }
         }
